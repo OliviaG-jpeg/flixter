380d142387cd58bdac8a8ea61d41eb5dba185e70
Olivia Goehrig <goehrigo@gmail.com>
2019-11-15 04:42:03 +0000

    images

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,6 +1,6 @@
 class Instructor::CoursesController < ApplicationController
-before_action :authenticate_user!
-before_action :require_authorized_for_current_course, only: [:show]
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def new
     @course = Course.new
@@ -11,7 +11,7 @@ before_action :require_authorized_for_current_course, only: [:show]
     if @course.valid?
      redirect_to instructor_course_path(@course)
     else
-      render :new, status: :unprocessable_entity
+     render :new, status: :unprocessable_entity
     end
   end
 
@@ -22,7 +22,7 @@ before_action :require_authorized_for_current_course, only: [:show]
 
   def require_authorized_for_current_course
     if current_course.user != current_user
-      render plain: 'Unauthorized', status: :unauthorized
+      render plain: "Unauthorized", status: :unauthorized
     end
   end
 
