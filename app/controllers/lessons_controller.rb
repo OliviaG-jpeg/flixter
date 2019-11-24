b3dd320749405b30d9f1b5d9cb3507067f355745
Olivia Goehrig <goehrigo@gmail.com>
2019-11-24 00:12:05 +0000

    drag

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_lessons
+
   def show
   end
 
   private
+
+  def require_authorized_for_lessons
+    if current_lesson.section.course.user != current_user
+      redirect_to course_path, alert: 'You must be enrolled in the course to view lessons'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
