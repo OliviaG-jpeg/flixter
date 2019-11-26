515497e6b6ff525a727d081a01f0784f272dc84e
Olivia Goehrig <goehrigo@gmail.com>
2019-11-26 17:11:21 +0000

    js

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -2,7 +2,8 @@ class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_course, only: [:create]
   before_action :require_authorized_for_current_section, only: [:update]
-
+  skip_before_action :verify_authenticity_token
+  
   def create
     @section = current_course.sections.create(section_params)
     redirect_to instructor_course_path(current_course)
